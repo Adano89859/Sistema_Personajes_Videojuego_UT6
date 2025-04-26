7eeecb38ebd9640b3ae1d64ed1a8af92c1f662a9
Adán Romero <adanromeromarrero@gmail.com>
2025-04-26 11:10:04 +0200

    Creación de la rama. Inicios de Hechicero, y Mago. Es necesario tener la interfaz de "Defendible" para crear la clase "Hechicero".

---
+++ src/Model/Hechicero.java
@@ -0,0 +1,10 @@
+package Model;
+
+/*
+
+AÑADIR LA INTERFACE DE "DEFENDIBLE" PARA SEGUIR TRABAJAMNDO ESTA CLASE
+
+ */
+
+public class Hechicero extends Personaje_Magico{
+}
---
+++ src/Model/Mago.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Mago extends Personaje_Magico implements Curable{
+
+    //Atributos:
+    private int mana;
+    private int sabiduria;
+
+    //Métodos:
+}
